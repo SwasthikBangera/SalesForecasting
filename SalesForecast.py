d1e389acac942806e9ce0e483f8472e07fca5f5f
Swasthik Bangera <swasthik.bangera@gmail.com>
2023-04-05 13:33:31 -0700

    First Update of Python file

---
+++ SalesForecast.py
@@ -0,0 +1,11 @@
+''' Create a Forecasting model for sales '''
+
+# Importing Dependencies
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+''' EDA '''
